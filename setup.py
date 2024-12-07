5ee02e48497da1557b2b3803e90a8bd1ec01e45d
fasdf <chenariel606@gmail.com>
2024-12-06 19:42:24 -0500

    debug

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mybook",
+    version="0.1",
+    install_requires=[
+        "sphinx",
+        "sphinx-book-theme",
+        "jupyter-sphinx",
+    ],
+)
